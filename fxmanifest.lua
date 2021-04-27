90bd24b89a3a8a47369ff49a6a296c9d4677e992
MasterkinG32 <lichwow_masterking@yahoo.com>
2021-04-27 13:04:10 +0430

    Cleanup.

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'adamant'
+game 'gta5'
+
+name 'master_banking'
+author 'MasterkinG32#9999'
+description 'MasterkinG32 Bank System!'
+version '1.0.0'
+
+ui_page 'client/html/UI.html'
+
+client_scripts {
+	'client/client.lua'
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'server/main.lua',
+}
+
+files {
+    'client/html/UI.html',
+    'client/html/style.css',
+	'client/html/img/phone.png',
+	'client/html/img/fingerprint.png',
+}
+
+dependencies {
+	'es_extended'
+}
